3739f6ddb53172904f72bf59bc2f1a3e5e28577c
theo.atlani@gmail.com <theo.atlani@gmail.com>
2022-01-09 08:50:23 +0100

    create game board

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+const baseWidth = 8
+const candyColors = ['blue', 'red', 'green', 'orange', 'purple', 'yellow']
+
+export default function App() {
+  const [currentColorBoard, setCurrentColorBoard] = useState([])
+
+  const createBoard = () =>  {
+    const colorsTab = []
+    for(let i = 0; i < baseWidth * baseWidth; i++) {
+      const randomColor = candyColors[Math.floor(Math.random() * candyColors.length)]
+      colorsTab.push(randomColor)
+    }
+    setCurrentColorBoard(colorsTab)
+  }
+
+  useEffect(() => {
+    createBoard()
+  }, [])
+
   return (
     <div className="App">
-      
+      <div className="game_container">
+        {currentColorBoard.map((candyColor, index) => (
+          <img style={{backgroundColor: candyColor}} alt={candyColor+'_fruit'} key={index} />
+        ))}
+      </div>
     </div>
   );
 }
-
-export default App;
